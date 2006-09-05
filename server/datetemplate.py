b598d47d2fbfa37eb73f7d301a855558fe1b14df
lostcontrol <lostcontrol@a942ae1a-1317-0410-a47c-b1dcaea8d605>
2006-09-05 21:17:35 +0000

    - Added date detector
    git-svn-id: https://fail2ban.svn.sourceforge.net/svnroot/fail2ban/trunk@326 a942ae1a-1317-0410-a47c-b1dcaea8d605

---
+++ server/datetemplate.py
@@ -0,0 +1,74 @@
+# This file is part of Fail2Ban.
+#
+# Fail2Ban is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Fail2Ban is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Fail2Ban; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# Author: Cyril Jaquier
+# 
+# $Revision: 321 $
+
+__author__ = "Cyril Jaquier"
+__version__ = "$Revision: 321 $"
+__date__ = "$Date: 2006-09-04 21:19:58 +0200 (Mon, 04 Sep 2006) $"
+__copyright__ = "Copyright (c) 2004 Cyril Jaquier"
+__license__ = "GPL"
+
+import re, time
+
+class DateTemplate:
+	
+	def __init__(self):
+		self.regex = ""
+		self.pattern = ""
+		self.hits = 0
+	
+	def setRegex(self, regex):
+		self.regex = regex
+		
+	def getRegex(self):
+		return self.regex
+	
+	def setPattern(self, pattern):
+		self.pattern = pattern
+		
+	def getPattern(self):
+		return self.pattern
+	
+	def isValid(self):
+		return self.regex != "" and self.pattern != ""
+	
+	def incHits(self):
+		self.hits = self.hits + 1
+	
+	def getHits(self):
+		return self.hits
+	
+	def matchDate(self, line):
+		dateMatch = re.search(self.regex, line)
+		return dateMatch
+	
+	def getDate(self, line):
+		date = None
+		dateMatch = self.matchDate(line)
+		if dateMatch:
+			date = list(time.strptime(dateMatch.group(), self.pattern))
+			if date[0] < 2000:
+				# There is probably no year field in the logs
+				date[0] = time.gmtime()[0]
+				# Bug fix for #1241756
+				# If the date is greater than the current time, we suppose
+				# that the log is not from this year but from the year before
+				if time.mktime(date) > time.time():
+					date[0] -= 1
+		return date
