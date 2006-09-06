1f4c181b0db5509142e0e75294f8b7b6774c799a
lostcontrol <lostcontrol@a942ae1a-1317-0410-a47c-b1dcaea8d605>
2006-09-06 18:25:11 +0000

    - Added "fail2ban-regex". This is a tool to help finding "failregex"
    git-svn-id: https://fail2ban.svn.sourceforge.net/svnroot/fail2ban/trunk@329 a942ae1a-1317-0410-a47c-b1dcaea8d605

--- setup.py
+++ setup.py
@@ -50,7 +50,8 @@ setup(
 	platforms = "Posix",
 	scripts =	[
 					'fail2ban-client',
-					'fail2ban-server'
+					'fail2ban-server',
+					'fail2ban-regex'
 				],
 	py_modules =	['version'],
 	packages =	[
