a49cc6bb08d11d4f964ba1fb4f7a068b14b4825b
Cyril Jaquier <cyril.jaquier@fail2ban.org>
2006-10-31 22:22:40 +0000

    - Added new date format (Apache). Thanks to Yaroslav Halchenko
    git-svn-id: https://fail2ban.svn.sourceforge.net/svnroot/fail2ban/trunk@439 a942ae1a-1317-0410-a47c-b1dcaea8d605

--- server/datedetector.py
+++ server/datedetector.py
@@ -61,6 +61,12 @@ class DateDetector:
 		template.setRegex("\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}")
 		template.setPattern("%Y/%m/%d %H:%M:%S")
 		self.__templates.append(template)
+		# Apache format [31/Oct/2006:09:22:55 -0000]
+		template = DateStrptime()
+		template.setName("Day/Month/Year:Hour:Minute:Second")
+		template.setRegex("\d{2}/\S{3}/\d{4}:\d{2}:\d{2}:\d{2}")
+		template.setPattern("%d/%b/%Y:%H:%M:%S")
+		self.__templates.append(template)
 		# TAI64N
 		template = DateTai64n()
 		template.setName("TAI64N")
