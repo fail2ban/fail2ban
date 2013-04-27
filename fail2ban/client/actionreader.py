d07df6637071b7ffa8be988a834a6bb71d9bf932
Steven Hiscocks <steven@hiscocks.me.uk>
2013-04-27 20:06:09 +0100

    NF: Allow setting of timeout for execution of action commands
    This uses subprocess.Popen, polling until `timeout` seconds has passed
    or the command has exit. If the command has not exited, fail2ban then
    sends SIGTERM, and if this is unsuccessful, SIGKILL.
    
    The timeout can be changed for an entire action via action [Init]
    options, or via jail.conf override, or fail2ban-client. The default
    timeout period is 60 seconds.

--- fail2ban/client/actionreader.py
+++ fail2ban/client/actionreader.py
@@ -61,8 +61,10 @@ class ActionReader(DefinitionInitConfigReader):
 				stream.append(head + ["actionban", self._file, self._opts[opt]])
 			elif opt == "actionunban":
 				stream.append(head + ["actionunban", self._file, self._opts[opt]])
-		# cInfo
 		if self._initOpts:
+			if "timeout" in self._initOpts:
+				stream.append(head + ["timeout", self._file, self._opts["timeout"]])
+			# cInfo
 			for p in self._initOpts:
 				stream.append(head + ["setcinfo", self._file, p, self._initOpts[p]])
 
