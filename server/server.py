a6f085786ca46485557c0adbe6b6510f1a06253b
Yaroslav Halchenko <debian@onerussian.com>
2013-11-24 09:50:39 -0500

    ENH: reintroducing levelnameinto syslog msgs, time stamp and indentation in non-syslog msgs
    any indentation from syslog msgs wsa removed -- no need

--- server/server.py
+++ server/server.py
@@ -347,10 +347,10 @@ class Server:
 		try:
 			self.__loggingLock.acquire()
 			# set a format which is simpler for console use
-			formatter = logging.Formatter("%(name)s[%(process)d]: %(levelname)-7s %(message)s")
+			formatter = logging.Formatter("%(asctime)s %(name)-16s[%(process)d]: %(levelname)-7s %(message)s")
 			if target == "SYSLOG":
 				# Syslog daemons already add date to the message.
-				formatter = logging.Formatter("%(name)s[%(process)d]: %(message)s")
+				formatter = logging.Formatter("%(name)s[%(process)d]: %(levelname)s %(message)s")
 				facility = logging.handlers.SysLogHandler.LOG_DAEMON
 				hdlr = logging.handlers.SysLogHandler("/dev/log", facility=facility)
 			elif target == "STDOUT":
