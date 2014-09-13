296a8403bfdc52eb18f8169e7fafccb3926fa7de
Vid Luther <vid@pressable.com>
2014-09-12 23:48:46 -0500

    removed changes to vagrant and stuff

--- provision.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo apt-get -y update
-sudo apt-get -y install curl python-systemd dh-systemd python-pip python-inotifyx git build-essential autotools-dev libssl-dev devscripts debhelper git libpcre3-dev libssl-dev
