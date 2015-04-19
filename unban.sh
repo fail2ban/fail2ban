79e1e9efe6263f3469ad769f6245d734dc59c126
kyle95wm <kyle95wm@gmail.com>
2015-04-19 19:53:42 -0400

    quick fix for unban.sh

--- unban.sh
+++ unban.sh
@@ -3,7 +3,7 @@ echo "This script is designed to manually un-ban IP addresses using iptables"
 echo
 echo
 iptables -L --line-numbers
-echo "Please enter the jail name (e.g: fail2ban-ssh)
+echo "Please enter the jail name (e.g: fail2ban-ssh)"
 read -e jail
 echo "Please enter the line num you want to unban"
 read -e line
