61ac48170308da590d317cac7e0b846461a86352
sebres <serg.brester@sebres.de>
2015-10-07 13:27:45 +0200

    IpToName test case fixed ('66.249.66.1' resp. 'crawl-66-249-66-1.googlebot.com' seems to be unresolvable)

--- fail2ban/tests/filtertestcase.py
+++ fail2ban/tests/filtertestcase.py
@@ -1025,8 +1025,8 @@ class DNSUtilsTests(unittest.TestCase):
 				self.assertEqual(res, [])
 
 	def testIpToName(self):
-		res = DNSUtils.ipToName('66.249.66.1')
-		self.assertEqual(res, 'crawl-66-249-66-1.googlebot.com')
+		res = DNSUtils.ipToName('8.8.4.4')
+		self.assertEqual(res, 'google-public-dns-b.google.com')
 		# invalid ip (TEST-NET-1 according to RFC 5737)
 		res = DNSUtils.ipToName('192.0.2.0')
 		self.assertEqual(res, None)
