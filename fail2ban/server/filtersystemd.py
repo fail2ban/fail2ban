fb27d9ce83191ecf2887b84453f7ea0dae9e4c8f
sebres <serg.brester@sebres.de>
2017-01-10 10:59:53 +0100

    fail2ban-regex: fix for systemd-journal (see gh-1657)

--- fail2ban/server/filtersystemd.py
+++ fail2ban/server/filtersystemd.py
@@ -179,6 +179,14 @@ class FilterSystemd(JournalFilter): # pragma: systemd no cover
 		return self.__matches
 
 	##
+	# Get journal reader
+	#
+	# @return journal reader
+
+	def getJournalReader(self):
+		return self.__journal
+
+	##
 	# Format journal log entry into syslog style
 	#
 	# @param entry systemd journal entry dict
