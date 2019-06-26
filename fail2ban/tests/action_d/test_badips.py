5bc8d73220831fdbb9abad4e427850c02ea31bc2
Sergey G. Brester <github@sebres.de>
2019-06-26 20:52:37 +0200

    test_badips.py: parameter `key` is removed in #2452

--- fail2ban/tests/action_d/test_badips.py
+++ fail2ban/tests/action_d/test_badips.py
@@ -83,7 +83,7 @@ if sys.version_info >= (2,7): # pragma: no cover - may be unavailable
 				'banaction': "test",
 				'age': "2w",
 				'score': 5,
-				'key': "fail2ban-test-suite",
+				#'key': "fail2ban-test-suite",
 				#'bankey': "fail2ban-test-suite",
 				'timeout': (3 if unittest.F2B.fast else 60),
 				})
